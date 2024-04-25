92324be049c0ff18c456c44eff619c7c4f704e69
ETK <temirlan200370@email.com>
2024-04-25 19:54:49 +0500

    coroutines

--- app/src/main/java/com/example/dz3/HabitViewModel.kt
+++ app/src/main/java/com/example/dz3/HabitViewModel.kt
@@ -8,13 +8,26 @@ import androidx.lifecycle.ViewModel
 class HabitViewModel(private val repository: HabitRepository) : ViewModel() {
     private var _query = ""
     private var _sort = SortOrder.ASCENDING
-    var habitListLiveData: MutableLiveData<List<Habit>> = MutableLiveData()
-    var goodHabitsLiveData: MutableLiveData<List<Habit>> = MutableLiveData()
-    var badHabitsLiveData: MutableLiveData<List<Habit>> = MutableLiveData()
 
-    init {
-        updateLiveData()
-    }
+    val habitListLiveData: LiveData<List<Habit>> = repository.allHabits
+    val goodHabitsLiveData: LiveData<List<Habit>> = repository.goodHabits
+    val badHabitsLiveData: LiveData<List<Habit>> = repository.badHabits
+
+
+//    var habitListLiveData: MutableLiveData<List<Habit>> = MutableLiveData()
+//    var goodHabitsLiveData: MutableLiveData<List<Habit>> = MutableLiveData()
+//    var badHabitsLiveData: MutableLiveData<List<Habit>> = MutableLiveData()
+
+//    init {
+//        repository.allHabits.observeForever { _ -> updateLiveData() }
+//        repository.goodHabits.observeForever { _ -> updateLiveData() }
+//        repository.badHabits.observeForever { _ -> updateLiveData() }
+//        habitListLiveData.value = repository.allHabits.value
+//        goodHabitsLiveData.value = repository.goodHabits.value
+//        badHabitsLiveData.value = repository.badHabits.value
+//
+//        updateLiveData()
+//    }
 
     suspend fun addHabit(habit: Habit) {
         repository.addHabit(habit)
@@ -46,13 +59,10 @@ class HabitViewModel(private val repository: HabitRepository) : ViewModel() {
         updateLiveData()
     }
 
-    fun updateLiveData() {
-        var a = repository.getAllHabitsFilteredSorted(_query, _sort).value
-        var b = repository.getHabitsByTypeFilteredSorted(HabitType.GOOD, _query, _sort).value
-        var c = repository.getHabitsByTypeFilteredSorted(HabitType.BAD, _query, _sort).value
-        habitListLiveData.value = repository.getAllHabitsFilteredSorted(_query, _sort).value
-        goodHabitsLiveData.value = repository.getHabitsByTypeFilteredSorted(HabitType.GOOD, _query, _sort).value
-        badHabitsLiveData.value = repository.getHabitsByTypeFilteredSorted(HabitType.BAD, _query, _sort).value
+    private fun updateLiveData() {
+//        habitListLiveData.value = repository.getAllHabitsFilteredSorted(_query, _sort).value
+//        goodHabitsLiveData.value = repository.getHabitsByTypeFilteredSorted(HabitType.GOOD, _query, _sort).value
+//        badHabitsLiveData.value = repository.getHabitsByTypeFilteredSorted(HabitType.BAD, _query, _sort).value
     }
 }
 
